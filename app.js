919691105561b2fce0b687e08eaaff6f11383be4
SENYa-408 <senya.s408@gmail.com>
2022-01-24 18:32:41 +0200

    REF: removed unused code blocks

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 const express = require("express");
-const engine = require("ejs-mate");
 const bodyParser = require("body-parser");
-const cors = require("cors");
-const path = require("path");
 
 const viewRoutes = require("./routes/viewRoutes");
 const userRoutes = require("./routes/userRoutes");
@@ -10,18 +7,8 @@ const santaReceiverRoutes = require("./routes/santaReceiverRoutes");
 
 const app = express();
 
-app.engine("ejs", engine);
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
-
-app.set("trust proxy", true);
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-app.use(express.static(path.join(__dirname, "public")));
-
-app.use(cors({origin: "http://localhost:3000"}));
 
 // ROUTES
 app.use("/", viewRoutes);
